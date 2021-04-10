1d62a6c08f2032abffae1f10bb4ee2ad5bbf9851
Vinicius Garcia <vinicius.garcia@sap.com>
2021-04-10 16:27:06 -0300

    Create portfolioWithEntropy.py

---
+++ portfolioWithEntropy.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+#import matplotlib.pyplot as plt
+import pandas as pd
+from pandas_datareader import data as wb
+from datetime import datetime, timedelta
+#from pandas.plotting import lag_plot, autocorrelation_plot, bootstrap_plot
+
+
+class FinancialDataForPeriod:
+    def __init__(self, assets, startDate, endDate ):
+        assetsData = {}
+        for asset in assets:
+        #calculando retornos logaritmicos direto na leitura
+            assetsData[asset] = np.log(1 + (wb.DataReader(asset, data_source='yahoo', start=startDate, end=endDate )['Adj Close'].pct_change() ) )
+        self.returns = pd.DataFrame(assetsData)
+        self.meanDailyReturns = pd.DataFrame(self.returns.mean()).reset_index(drop=True)
+        self.meanDailyReturns.index = assets
+        self.covMatrix = self.returns.cov()
+
+
+assets = ['mglu3', 'prio3', 'bpac3', 'tots3', 'wege3']
+assets = [asset+'.sa' for asset in assets]
+delta = timedelta(days = 1200)
+startDate = (datetime.today() - delta).isoformat()
+
+testperiod = FinancialDataForPeriod(assets, startDate, datetime.today())
+
+
+
+
+def shannonEntropy( portfolio ):
+    entropy = 0
+    for weight in portfolio:
+        entropy -= weight*np.log(weight)
+    return entropy
+
+class MetropolisAlgorithm:
+    def __init__(self, covmatrix):
+
+        self.covmatrix = covmatrix
+
+    def __costFunction( ):
+        return 0
+
+    def metropolis( weights ):
+        self.weights = weights
+        self.__costFunction( )
+        for weight in self.weights:
+            weight += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
